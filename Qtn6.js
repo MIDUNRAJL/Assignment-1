d89e7d8ca602b2ad04b97536bdbf10deb61a928c
MIDUNRAJL <135823975+MIDUNRAJL@users.noreply.github.com>
2023-07-12 12:24:17 +0530

    completed

---
+++ Qtn6.js
@@ -0,0 +1,8 @@
+let price_1 = 100;
+let price_2 = 200;
+let price_3 = 300;
+let quantity_1 = 2;
+let quantity_2 = 3;
+let quantity_3 = 5;
+let total_amount = (price_1*quantity_1) + (price_2*quantity_2) + (price_3*quantity_3);
+console.log("Total amount is "+total_amount);
