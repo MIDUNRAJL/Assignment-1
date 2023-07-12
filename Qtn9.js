424c0b11e8f2441100dc497b9b8a5c1bf4f3dd5f
MIDUNRAJL <135823975+MIDUNRAJL@users.noreply.github.com>
2023-07-12 14:16:39 +0530

    completed

---
+++ Qtn9.js
@@ -0,0 +1,23 @@
+let size_1 = 'S';
+let size_2 = 'M';
+let size_3 = 'L';
+let size_4 = 'XL';
+var name = 'S';
+if(name== 'S'){
+    console.log("Small")
+}
+else if(name == 'M')
+{
+    console.log('Medium')
+}
+else if(name == 'L' )
+{
+    console.log('Large')
+}
+else if(name == 'XL')
+{
+    console.log('Extra Large')
+}
+else{
+    console.log("None");
+}
