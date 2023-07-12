a1ea6a8e6070d341e65a9450e10ae5f129683c8b
MIDUNRAJL <135823975+MIDUNRAJL@users.noreply.github.com>
2023-07-12 15:42:41 +0530

    completed

---
+++ Qtn11.js
@@ -0,0 +1,8 @@
+let name = 'Midun';
+let vari = 5;
+while(vari>0)
+{
+    console.log(name);
+    vari--;
+
+}
---
+++ Qtn13.js
@@ -0,0 +1,6 @@
+let name = "Come on guys";
+for(i=0;i<name.length;i++)
+if(name[i]!='a'&& name[i]!='A'&& name[i]!='e'&& name[i]!='E'&& name[i]!='i'&& name[i]!='I'&& name[i]!='o'&& name[i]!='O'&& name[i]!='u'&& name[i]!='U')
+{
+    console.log(name[i]);
+}
