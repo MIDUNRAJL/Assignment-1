91192c08b59fd144f6319a53f9cda14ee739a384
MIDUNRAJL <135823975+MIDUNRAJL@users.noreply.github.com>
2023-07-12 11:40:06 +0530

    completed

---
+++ Qtn4.js
@@ -0,0 +1,6 @@
+let length = 15;
+let width = 5;
+let perimeter = 2*(length + width);
+let area = length*width;
+console.log("The perimeter of rectangle "+perimeter);
+console.log("The area of rectangle "+area);
